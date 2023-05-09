6ef3f1430967ac26f2ce7be3dd0375aa4ef793ff
muhammadcess <rizqismuhamad@gmail.com>
2023-05-09 13:59:10 +0700

    first commit

---
+++ strpos.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+    <?php
+    $strpos = "Belajar PHP menggunakan strpos()";
+    $searchstrpos = "strpos()";
+    echo "\n";
+    echo strpos($strpos, $searchstrpos); // hasilnya 12
+    ?>
+
+//tugas 1
+    <?php
+    $strpos = "saat ini saya sudah mencapai PHP()";
+    $searchstrpos = "PHP()";
+    echo "\n";
+    echo strpos($strpos, $searchstrpos); // hasilnya 12
+    ?>
+
+//tugas 2
+    <?php
+    $strpos = "Aku telah melalui tahapan belajar php string, sekarang telah mencapai tahapan strpos";
+    $searchstrpos = "belajar php string";
+    echo "\n";
+    echo strpos($strpos, $searchstrpos); // hasilnya 12
+    ?>
+</body>
+
+</html>
---
+++ strreplace.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+
+<body>
+    <?php
+    $karakter = "Belajar PHP menggunakan strlen()";
+    echo "karakter sebelum di ubah: " . $karakter;
+    echo "\n";
+    echo "karakter setelah di ubah: " . str_replace($karakter, "strlen", "str_replace");
+    ?>
+
+    //tugas
+    <?php
+    $karakter = "kita sedang Belajar JAVASCRIPT";
+    echo "karakter sebelum di ubah: " . $karakter;
+    echo "\n";
+    echo "karakter setelah di ubah: " . str_replace($karakter, "JAVASCRIPT", "PHP");
+    ?>
+</body>
+
+</html>
